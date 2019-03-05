dfcc6842367364a272964dd727573619641ce7dc
prembhusal <remp.bhusal@gmail.com>
2019-03-05 15:12:59 -0500

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+#plot the output for localMst vs SparkMst
+
+x = [2,4,8,16,32,64]
+local = [1,2,6,21,79,302]
+par = [18,19,22,30,43,77]
+
+
+plt.scatter(x,local)
+plt.plot(x,local,linewidth = 1.5, label = 'localMST', color = 'red')
+
+plt.scatter(x,par)
+plt.plot(x,par,linewidth = 1.5, label = 'SparkMST', color = 'green')
+
+plt.xlabel("Junction sequence size in thousands")
+plt.ylabel("Time in seconds")
+plt.legend(loc="upper left")
+plt.show()
