e90ac115d41c9d4e96b04e1bd66ac1028a63bd72
prembhusal <remp.bhusal@gmail.com>
2019-03-30 17:49:59 -0400

    Update countFileLines.py

--- countFileLines.py
+++ countFileLines.py
@@ -1,1 +1,9 @@
 #this program will list all the files inside the folder and count the total lines in each file
+import os
+
+arr = os.listdir('.') # list of files inside current directory
+
+#clunt Total line for each file
+for fileName in arr:
+	#print fileName 
+	os.system("wc -l " +fileName)
