e9170e1f25f00bf6b02eca4743772d7eef81300d
prembhusal <remp.bhusal@gmail.com>
2019-04-04 21:10:09 -0400

    Update regressionLine.py

--- regressionLine.py
+++ regressionLine.py
@@ -9,7 +9,7 @@ y = [1452,2690,1123,8791]
 #x = mul(x)
 #plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)), color = "green", linestyle="--")
 p1 =np.polyfit(x,y,1)
-x_extended = np.linspace(20, 2000, 100)
+x_extended = np.linspace(20, 2000, 100) # for making extended line beyond exact values
 plt.plot(x_extended,np.polyval(p1,x_extended),'r-', linestyle="--")
 plt.scatter(147,1452,color = "red",label ='D4')
 plt.scatter(164,2690,color='orange',label='D1')
