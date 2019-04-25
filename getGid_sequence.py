6a8e262afc62c8023c0bad86f165c77f4cd6cf67
prembhusal <remp.bhusal@gmail.com>
2019-04-24 23:09:49 -0400

    Update getGid_sequence.py

--- getGid_sequence.py
+++ getGid_sequence.py
@@ -1,1 +1,25 @@
 
+import sys
+
+# program to generate groupid based on common v-gene,j-gene and junction length
+# Generate (gid, sequence)
+# sequence = index, seqId, Junction
+infile = sys.argv[1] # input dataset file
+
+outfile = sys.argv[2] # name for output file
+
+def getGroupIdSequence():
+	data = pd.read_csv(infile,sep='\t')
+
+	# #keep only the first value from v and j column
+	data["V_CALL"] = data["V_CALL"].str.split(",").str[0]
+	data["J_CALL"] = data["J_CALL"].str.split(",").str[0]
+	data['Index'] = data.index
+	data["Sequence"] = data["Index"].astype(str)+","+data["SEQUENCE_ID"].astype(str)+","+data["JUNCTION"].astype(str)
+	data["groupId"] = data.groupby(["V_CALL","J_CALL","JUNCTION_LENGTH"]).grouper.group_info[0]
+
+	data1 = data[["groupId","Sequence"]]
+	data1.to_csv(outfile+".tsv", sep='\t',index=False)
+
+
+
